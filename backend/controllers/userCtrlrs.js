0ccfa873e44d94b2ffb3520f706ead10d1d4ec64
Mbaye <mmbaye1@hotmail.fr>
2022-04-11 13:53:33 +0200

    mise en place du fichier userCntrls et importation

---
+++ backend/controllers/userCtrlrs.js
@@ -0,0 +1,4 @@
+//============================== Importations userModel , bcrypt & jwt =================================
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userModel = require("../models/user");
