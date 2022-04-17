9adeda12160fd0f7d48708cd8794fc6088bbabc0
Mbaye <mmbaye1@hotmail.fr>
2022-04-17 17:36:28 +0200

    Implementation du module multer-config ainsi que  son exportation,importattion et son application sur la route updateProfile pour prendre en compte les fichier images

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,11 +6,14 @@ const router = express.Router();
 //Importation du moule uerControllers
 const userCtrlrs = require("../controllers/userCtrlrs");
 
+//Importation de multer
+const multer = require("../middlewares/multer-confi");
+
 //=================================================== Les endpoints ===========================================================
 router.post("/register", userCtrlrs.register);
 router.post("/login", userCtrlrs.login);
 router.get("/profile/:id", userCtrlrs.getUserProfile);
-router.put("/profile/:id", userCtrlrs.updateProfile);
+router.put("/profile/:id", multer, userCtrlrs.updateProfile);
 router.delete("/profile/:id", userCtrlrs.deleteProfile);
 //====================================================== Exportation de router ================================================
 module.exports = router;
