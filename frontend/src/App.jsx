c972ebd2424a70fdaaeb17fd2aa2ead9213b6061
Mbaye <mmbaye1@hotmail.fr>
2022-05-03 19:53:39 +0200

    Mise en place du squelette du front et installation de react-router-dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../src/styles/app.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="title"> Je suis dans app.js</h1>
+    </div>
+  );
+};
+
+export default App;
